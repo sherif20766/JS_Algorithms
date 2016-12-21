d53445abb967d9bfac3058eab6d8e64674c2a23d
Sherif Shawky <sherif20766@hotmail.com>
2016-12-21 14:01:44 +0000

    completed

---
+++ Intermediate/Diff_two_arrays.js
@@ -0,0 +1,19 @@
+/*Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. 
+In other words return the symmetric difference of the two arrays.*/
+
+function diffArray(arr1, arr2) {
+  var newArr = (arr1.concat(arr2)).sort(function(a,b){return a-b;});
+  var answer = [];
+  
+  for(var i=0;i<newArr.length;i++){
+        
+   if(newArr.indexOf(newArr[i]) == newArr.lastIndexOf(newArr[i])){
+     answer.push(newArr[i]);   
+   }  
+  
+  }
+  
+  return answer;
+}
+
+diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]);
