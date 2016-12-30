a06eb6589939fbe3c52ebed0fcd70d3af7f1fd3a
sherif20766 <sherif20766@hotmail.com>
2016-12-30 22:18:38 +0000

    initial commit

---
+++ Intermediate/Missing_Letters.js
@@ -0,0 +1,14 @@
+
+function fearNotLetter(str) {
+  var array=[];
+  
+  for(var i=0;i<str.length;i++){
+    array.push(str.charCodeAt(i));
+  
+  
+  }
+return array;
+
+}
+
+fearNotLetter("abce");
